43f3834baf295f4b5fd77edaccac1db696c116db
Vipul Bijalwan <vipulbijalwan112@gmail.com>
2026-03-06 10:46:10 +0530

    Learning Airflow

--- learning_airflow_2/dags/5_xcoms_kwargs.py
+++ learning_airflow_2/dags/5_xcoms_kwargs.py
@@ -3,32 +3,45 @@ from datetime import datetime
 
 
 @dag(
-    dag_id="xcoms_dag_auto",
+    dag_id="xcoms_dag_manual",
     
 )
-def xcoms_dag_auto():
+def xcoms_dag_manual():
 
     @task
-    def first_task():
+    def first_task(**kwargs):
+
+        # Extracting "ti" from kwargs
+
+        ti = kwargs['ti']
         print("Extracting data....")
         fetched_data = {"data": [1, 2, 3, 4, 5, 6]}
+        ti.xcom_push(key="return_result",value=fetched_data)
         return fetched_data
 
     @task
-    def second_task(data: dict):
-        fetched_data = data['data']
+    def second_task(**kwargs):
+
+        ti=kwargs['ti']
+
+        fetched_data = ti.xcom_pull(task_ids="first_task",key='return_result')['data']
         transformed_data = [x * 2 for x in fetched_data]
         transformed_data_dict = {"transf_data": transformed_data}
+        
+        ti.xcom_push(key="transformed_data_dict",value=transformed_data_dict)
         return transformed_data_dict
 
     @task
-    def third_task(data: dict):
-        print("Loading data:", data)
+    def third_task(**kwargs):
+        ti=kwargs['ti']
+        ti.xcom_push(key)
         return data
 
     first = first_task()
-    second = second_task(first)
-    third = third_task(second)
+    second = second_task()
+    third = third_task()
+
+    first >> second >> third
 
 
 dag = xcoms_dag_auto()
