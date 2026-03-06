c0712371fa50337c003b38e36acc511b288e302c
Vipul Bijalwan <vipulbijalwan112@gmail.com>
2026-03-06 22:18:40 +0530

    Airflow images

--- learning_airflow_2/dags/6_parallel_tesk copy.py
+++ learning_airflow_2/dags/6_parallel_tesk copy.py
@@ -1,4 +1,4 @@
-from airflow.decorators import dag, task
+from airflow.sdk import dag, task
 from datetime import datetime
 
 
@@ -13,18 +13,16 @@ def parallel_dag():
     @task
     def extract_task(**kwargs):
 
-        print("EXT DATA.......")
-
         ti = kwargs['ti']
 
         extracted_data_dict = {
-            "api_extracted_data": [1, 2, 3, 4],
-            "db_extracted_data": [4, 5, 6, 7],
-            "s3_extracted_data": [7, 8]
+            "api_extracted_data": [1,2,3,4],
+            "db_extracted_data": [4,5,6,7],
+            "s3_extracted_data": [7,8]
         }
 
         ti.xcom_push(
-            key='return_result',
+            key="extracted_data_dict",
             value=extracted_data_dict
         )
 
@@ -34,15 +32,14 @@ def parallel_dag():
 
         ti = kwargs['ti']
 
-        api_extracted_data = ti.xcom_pull(
-            task_ids="extract_task"
-        )["api_extracted_data"]
+        extracted_data_dict = ti.xcom_pull(
+            task_ids="extract_task",
+            key="extracted_data_dict"
+        )
 
-        print("Transforming API data....")
+        api_extracted_data = extracted_data_dict["api_extracted_data"]
 
-        transformed_api_data = [
-            x * 100 for x in extracted_data_dict['api_extracted_data']
-        ]
+        transformed_api_data = [x * 100 for x in api_extracted_data]
 
         ti.xcom_push(
             key="transformed_api_data",
@@ -59,8 +56,6 @@ def parallel_dag():
             task_ids="extract_task",
             key="extracted_data_dict"
         )
-
-        print("Transforming DB data....")
 
         transformed_db_data = [
             x * 100 for x in extracted_data_dict['db_extracted_data']
@@ -81,8 +76,6 @@ def parallel_dag():
             task_ids="extract_task",
             key="extracted_data_dict"
         )
-
-        print("Transforming S3 data....")
 
         transformed_s3_data = [
             x * 100 for x in extracted_data_dict['s3_extracted_data']
@@ -114,16 +107,17 @@ def parallel_dag():
             key="transformed_s3_data"
         )
 
-        print("Loading data....")
-
         print(api_data)
         print(db_data)
         print(s3_data)
 
+
     extract = extract_task()
+
     transform_api = transform_task_api()
     transform_db = transform_task_db()
     transform_s3 = transform_task_s3()
+
     load = load_task()
 
     extract >> [transform_api, transform_db, transform_s3] >> load
--- learning_airflow_2/dags/7_condition_task.py
+++ learning_airflow_2/dags/7_condition_task.py
@@ -1,31 +1,31 @@
-from airflow.decorators import dag, task
+from airflow.sdk import dag, task
 from datetime import datetime
 
 
 @dag(
-    dag_id="parallel_dag",
+    dag_id="condition_dag",
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False
 )
-def parallel_dag():
+def condition_dag():
 
     @task
     def extract_task(**kwargs):
 
-        print("EXT DATA.......")
-
         ti = kwargs['ti']
 
         extracted_data_dict = {
-            "api_extracted_data": [1, 2, 3, 4],
-            "db_extracted_data": [4, 5, 6, 7],
-            "s3_extracted_data": [7, 8]
+            "api_extracted_data": [1,2,3,4],
+            "db_extracted_data": [4,5,6,7],
+            "s3_extracted_data": [7,8],
+            "weekend_flag": "false"
         }
 
         ti.xcom_push(
-            key="extracted_data_dict"
-        )['extracted_data_dict'] 
+            key="extracted_data_dict",
+            value=extracted_data_dict
+        )
 
 
     @task
@@ -34,14 +34,13 @@ def parallel_dag():
         ti = kwargs['ti']
 
         extracted_data_dict = ti.xcom_pull(
-            task_ids="extract_task"
-        )["extracted_data_dict"]
+            task_ids="extract_task",
+            key="extracted_data_dict"
+        )
 
-        print("Transforming API data....")
+        api_extracted_data = extracted_data_dict["api_extracted_data"]
 
-        transformed_api_data = [
-            x * 100 for x in extracted_data_dict['api_extracted_data']
-        ]
+        transformed_api_data = [x * 100 for x in api_extracted_data]
 
         ti.xcom_push(
             key="transformed_api_data",
@@ -58,8 +57,6 @@ def parallel_dag():
             task_ids="extract_task",
             key="extracted_data_dict"
         )
-
-        print("Transforming DB data....")
 
         transformed_db_data = [
             x * 100 for x in extracted_data_dict['db_extracted_data']
@@ -81,8 +78,6 @@ def parallel_dag():
             key="extracted_data_dict"
         )
 
-        print("Transforming S3 data....")
-
         transformed_s3_data = [
             x * 100 for x in extracted_data_dict['s3_extracted_data']
         ]
@@ -91,6 +86,25 @@ def parallel_dag():
             key="transformed_s3_data",
             value=transformed_s3_data
         )
+
+
+    # Branch task
+    @task.branch
+    def condition_task(**kwargs):
+
+        ti = kwargs['ti']
+
+        extracted_data_dict = ti.xcom_pull(
+            task_ids="extract_task",
+            key="extracted_data_dict"
+        )
+
+        weekend_flag = extracted_data_dict["weekend_flag"]
+
+        if weekend_flag == "true":
+            return "no_load_task"
+        else:
+            return "load_task"
 
 
     @task
@@ -113,24 +127,29 @@ def parallel_dag():
             key="transformed_s3_data"
         )
 
-        print("Loading data....")
-
         print(api_data)
         print(db_data)
         print(s3_data)
 
 
+    @task
+    def no_load_task():
+
+        print("No loading today, it's weekend!")
+
+
     extract = extract_task()
 
     transform_api = transform_task_api()
-
     transform_db = transform_task_db()
-
     transform_s3 = transform_task_s3()
 
+    condition = condition_task()
+
     load = load_task()
+    no_load = no_load_task()
 
-    extract >> [transform_api, transform_db, transform_s3] >> load
+    extract >> [transform_api, transform_db, transform_s3] >> condition >> [load, no_load]
 
 
-parallel_dag()
+condition_dag()
